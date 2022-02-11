13d473afab22c988831986503fd6d44fe422a661
j6512 <justins8287@gmail.com>
2022-02-10 23:33:39 -0600

    beginning

---
+++ src/main/java/com/j6512/todolist_manager/TodolistManagerApplication.java
@@ -0,0 +1,13 @@
+package com.j6512.todolist_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodolistManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodolistManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/j6512/todolist_manager/TodolistManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.j6512.todolist_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
